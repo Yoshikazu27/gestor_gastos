91ef0bf1bd7479f65585c807d62edbd5e0dc2446
Yoshikazu Casanova <yoshikazu.casanova@gmail.com>
2026-05-21 22:53:59 -0500

    Cambios en caché

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gastos-app-v1";
+const CACHE_NAME = "gastos-app-v2";
 
 const urlsToCache = [
   "./",
@@ -11,6 +11,8 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(async cache => {
       for (const url of urlsToCache) {
@@ -22,6 +24,20 @@ self.addEventListener("install", event => {
       }
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
